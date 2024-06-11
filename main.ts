98f6a9faab67386f5f61d668f71a30b27b6d6609
KimTrxy <159698556+KimTrxy@users.noreply.github.com>
2024-06-12 02:13:41 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { load } from 'https://deno.land/std@0.224.0/dotenv/mod.ts'
 
 const env = await load()
 const userID = env['UUID'] || '0934518d-1a9a-486b-b675-3b0951ae8674'
-const proxyIP = env['PROXYIP'] || 'tencent.rin-tohsakagangbang.filegear-sg.me'
+const proxyIP = env['PROXYIP'] || ''
 
 if (!isValidUUID(userID)) {
   throw new Error('uuid is not valid')
