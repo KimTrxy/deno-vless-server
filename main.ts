25d4e52515374aa297886d82abcae4eb221a178e
KimTrxy <159698556+KimTrxy@users.noreply.github.com>
2024-06-12 02:01:44 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { load } from 'https://deno.land/std@0.224.0/dotenv/mod.ts'
 
 const env = await load()
-const userID = env['UUID'] || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
-const proxyIP = env['PROXYIP'] || ''
+const userID = env['UUID'] || '0934518d-1a9a-486b-b675-3b0951ae8674'
+const proxyIP = env['PROXYIP'] || 'tencent.rin-tohsakagangbang.filegear-sg.me'
 
 if (!isValidUUID(userID)) {
   throw new Error('uuid is not valid')
@@ -18,7 +18,7 @@ Deno.serve(async (request: Request) => {
     switch (url.pathname) {
       case '/':
         return new Response('Hello, world!')
-      case `/${userID}`: {
+      case `/worldwars`: {
         const vlessConfig = getVLESSConfig(userID, url.hostname, url.port || (url.protocol === 'https' ? 443 : 80))
         return new Response(`${vlessConfig}`, {
           status: 200,
